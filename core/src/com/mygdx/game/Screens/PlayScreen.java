9bbdcb190e52c13ea5db4787e3c3eee85528f02d
aletofo <atofoni6@gmail.com>
2021-10-11 18:05:30 +0200

    -definizioni di base per gli oggetti del mondo di gioco (una volta scelti pack da usare su Tiled e create le mappe, si possono iniziare ad implementare gli oggetti anche su IntelliJ, vedi classe B2WorldCreator di MarioBros) -metodi update e handle input aggiunti nel PlayScreen

--- core/src/com/mygdx/game/Screens/PlayScreen.java
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -1,15 +1,20 @@
 package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Scenes.Hud;
+import com.mygdx.game.Tools.B2WorldCreator;
 
 public class PlayScreen implements Screen {
 
@@ -20,6 +25,12 @@ public class PlayScreen implements Screen {
 
     private Hud hud;
 
+    //variabili per la creazione del mondo di gioco
+    private World world;
+    private Box2DDebugRenderer b2dr;
+    private B2WorldCreator creator;
+
+    //sprite di prova per i cuori
     private Sprite hearts;
 
     public PlayScreen(MyGdxGame game){
@@ -32,7 +43,13 @@ public class PlayScreen implements Screen {
                 gamecam);
         hud = new Hud(game.batch);
 
-        hearts = new Sprite(new Texture(Gdx.files.internal("hearts.png")));
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+        world = new World(new Vector2(0, -10), true);
+        b2dr = new Box2DDebugRenderer();
+        creator = new B2WorldCreator(this);
+
+        //hearts = new Sprite(new Texture(Gdx.files.internal("hearts.png")));
     }
 
     @Override
@@ -40,15 +57,27 @@ public class PlayScreen implements Screen {
         // TODO Auto-generated method stub
     }
 
+    //method useful to catch the keyboard inputs
+    public void handleInput(float dt){
+
+    }
+
+    //we will call this method in our render method
+    public void update(float dt) {
+        handleInput(dt);
+    }
+
     @Override
     public void render(float delta) {
         // TODO Auto-generated method stub
+        update(delta);
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         game.batch.setProjectionMatrix(gamecam.combined);
         hud.stage.draw();
         game.batch.begin();
-        hearts.draw(game.batch);
+        //hearts.draw(game.batch);
         game.batch.end();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
@@ -57,6 +86,10 @@ public class PlayScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         // TODO Auto-generated method stub
+    }
+
+    public World getWorld(){
+        return world;
     }
 
     @Override
--- core/src/com/mygdx/game/Tools/B2WorldCreator.java
+++ core/src/com/mygdx/game/Tools/B2WorldCreator.java
@@ -1,4 +1,17 @@
 package com.mygdx.game.Tools;
 
+import com.badlogic.gdx.physics.box2d.*;
+import com.mygdx.game.Screens.PlayScreen;
+
 public class B2WorldCreator {
+
+    public B2WorldCreator(PlayScreen screen){
+        World world = screen.getWorld();
+
+        //definizioni generiche degli elementi che servono ad implementare tutti gli oggetti del mondo di gioco
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+    }
 }
