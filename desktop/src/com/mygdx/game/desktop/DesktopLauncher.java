9473d65099ca45db520d386b33ae071aa7506ab1
aletofo <atofoni6@gmail.com>
2021-10-15 16:59:18 +0200

    -risolto problema viewport -fullscreen di default

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-
+		//default fullscreen
 		config.width = 1024;
 		config.height = 768;
 		config.fullscreen = true;
