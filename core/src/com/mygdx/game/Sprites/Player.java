8557422b4b3c744551c5e90a7d5dd03e033c8ff0
aletofo <atofoni6@gmail.com>
2021-10-22 15:51:17 +0200

    -aggiunti metodi e commenti nella classe Player

--- core/src/com/mygdx/game/Sprites/Player.java
+++ core/src/com/mygdx/game/Sprites/Player.java
@@ -9,15 +9,37 @@ import com.mygdx.game.Screens.PlayScreen;
 
 public class Player extends Sprite {
 
+    //stati in cui si può trovare il Player. Utili per distinguere le animazioni da assegnare
+    public enum State { FALLING, JUMPING, STANDING, RUNNING}
+    public State currentState;
+    public State previousState;
+
     public World world;
     public Body b2body;
+
+    //run e jump sono Animation di TextureRegion, in quanto serve piu di una TextureRegion per creare l'animazione
+    //del movimento e del salto. Per stand basta una sola TextureRegion (una sola immagine praticamente)
     private TextureRegion stand;
+    private Animation<TextureRegion> run;
+    private Animation<TextureRegion> jump;
+    //timer che indica il tempo speso in un certo stato (RUNNING,JUMPING, etc.),
+    //rappresentato dalla corrispondente animazione(vedi il suo utilizzo come parametro nello switch del metodo getFrame)
+    private float stateTimer;
+    //boolean utile a capire il verso in cui corre il Player e, di conseguenza, ad orientare la sua sprite
+    private boolean runningRight;
 
     public Player(World world, PlayScreen screen){
 
+        //seleziono la regione di texture da cui partire per andare a costruire le varie animazioni
+        //la stringa inserita come parametro è presente nel file 'RedKnight.pack' e ce n'è una per ogni regione
         super(screen.getAtlas().findRegion("knight death animation"));
         //setto il mondo di gioco del PlayScreen
         this.world = screen.getWorld();
+
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
 
         //chiamo la funzione definePlayer() che definisce tutte le caratteristiche del corpo del Player
         definePlayer();
@@ -30,6 +52,56 @@ public class Player extends Sprite {
     public void update(float dt){
         setPosition(b2body.getPosition().x - getWidth() / 2,
                 b2body.getPosition().y - getHeight() / 2);
+        //setto la regione in base alla serie di frame che sono ritornati dal metodo getFrame
+        setRegion(getFrame(dt));
+    }
+
+    //metodo per la selezione dei frame corrispondenti allo stato corrente e per l'aggiornamento dello stateTimer
+    public TextureRegion getFrame(float dt){
+        //metto in currentState lo stato che ottengo dal metodo getState
+        currentState = getState();
+
+        TextureRegion region;
+        switch(currentState){
+            case JUMPING:
+                region = jump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = run.getKeyFrame(stateTimer, true);//il parametro looping va messo a true solo se
+                                                                  //l'animazione va ripetuta(es. RUNNING)
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = stand;
+                break;
+        }
+        //if utili a settare il verso della texture tramite il boolean runningRight
+        if((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()){
+            region.flip(true, false);
+            runningRight = false;
+        }
+        else if((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()){
+            region.flip(true, false);
+            runningRight = true;
+        }
+
+        //aggiorno lo state timer--> se (currentState == previousState) --> stateTimer += dt;
+        //else --> stateTimer = 0;
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        return region;
+    }
+
+    //metodo che seleziona lo stato corrente in base alla velocita del Player
+    public State getState(){
+        if(b2body.getLinearVelocity().y > 0 || (b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING))
+            return State.JUMPING;
+        else if(b2body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if(b2body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        else return State.STANDING;
     }
 
     public void definePlayer(){
