856bc7cdca6b25092c31add46be40b679dc3656a
PinelliLuca <p.lucapinelli@gmail.com>
2021-10-25 13:17:49 +0200

    Errori dopo il merge: Sistemata classe player e piccolo errore in bow, NON risolto playScreen in gameover

--- core/src/com/mygdx/game/Sprites/Player.java
+++ core/src/com/mygdx/game/Sprites/Player.java
@@ -14,6 +14,7 @@ import com.mygdx.game.Weapon.Weapon;
 
 public class Player extends Sprite {
 
+
     public Weapon getCurrentWeapon() {
         return CurrentWeapon;
     }
--- core/src/com/mygdx/game/Weapon/Bow.java
+++ core/src/com/mygdx/game/Weapon/Bow.java
@@ -3,7 +3,7 @@ package com.mygdx.game.Weapon;
 public class Bow extends Weapon{
     public Bow(String name, int damage) {
         super(name, damage);
-        name="Arco"
+        name="Arco";
         damage=10;
     }
 }
