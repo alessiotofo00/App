034b8536ef341d8d1642b9e7ae2f58264ea9b903
PinelliLuca <p.lucapinelli@gmail.com>
2022-01-01 23:44:09 +0100

    Level7

--- core/assets/dungeon 16x16.tsx
+++ core/assets/dungeon 16x16.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="dungeon 16x16" tilewidth="16" tileheight="16" tilecount="12288" columns="128">
+<tileset version="1.5" tiledversion="1.7.2" name="dungeon 16x16" tilewidth="16" tileheight="16" tilecount="12288" columns="128">
  <image source="DungeonCrawl_ProjectUtumnoTileset.png" width="2048" height="1536"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="4910" duration="100"/>
+   <frame tileid="5038" duration="100"/>
+   <frame tileid="5039" duration="100"/>
+   <frame tileid="4911" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
